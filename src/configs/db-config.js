3b446101a27a30c183390b3248bba8d6158ccf64
IAN MANUEL SEGADE <48521796@ort>
2025-07-10 12:06:47 -0300

    conexion exitosa a la bd

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -4,8 +4,8 @@ const config = {
   host: "aws-0-sa-east-1.pooler.supabase.com",
   port: 6543,
   database: "postgres",
-  user: "postgres.wjqvqngievjqjsceogqa",
-  password: "skibidiMortis",
+  user: "postgres.ryvwoillpxdgdfogkdbq",
+  password: "SkibidiMortis2",
 };
 
 const client = new Client(config);
--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
 import express from "express";
 import cors from "cors";
-// import config from "./config/db-config.js";
 import pkg, {Pool} from "pg";
-import multer from "multer";
-import client from "./documents/database/client.js";
-// import publicacionRoutes from './routes/publicacionRoutes.js';
+import client from "./configs/db-config.js";
 
 const { Client } = pkg;
 const app = express();
 const PORT = 3000;
-
+client.connect();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/prueba", async (req, res) => {
-  try {
-    // Llamamos a la función prueba que devuelve JSON
-    const { rows } = await client.query("SELECT * from pruebaa()");
-    // rows[0].resultado es el JSON que devuelve la función
-    res.json(rows.resultado);
-  } catch (error) {
-    console.error("Error ejecutando la función prueba", error);
-    res.status(500).json({ error: "Error ejecutando la función prueba" });
-  }
+app.get("/prueba", async (req, res) => {
+  const response = await client.query('SELECT * from pruebaa()');
+  res.send(response.rows);
 });
-
 
 
 app.listen(PORT, () => {
